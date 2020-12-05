50e5f9dadd7fd2fb9ffc8f50d11a779316855af9
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2020-12-05 19:55:36 +0100

    Day 4

---
+++ Day4/Passport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day4
+{
+    public class Passport
+    {
+        private Dictionary<string, string> fields = new Dictionary<string, string>();
+
+        public void AddField(string key, string value) => fields.Add(key, value);
+
+        public bool Validate() =>
+            fields.Where(x => ValidateField(x.Key, x.Value)).Count() is 7;
+
+        private bool ValidateField(string key, string value) =>
+            key switch
+            {
+                "byr" => int.Parse(value) is >= 1920 and <= 2002,
+                "iyr" => int.Parse(value) is >= 2010 and <= 2020,
+                "eyr" => int.Parse(value) is >= 2020 and <= 2030,
+                "hgt" => value.Contains("cm") ? int.Parse(value[0..^2]) is >= 150 and <= 193 
+                        : value.Contains("in") ? int.Parse(value[0..^2]) is >= 59 and <= 76
+                        : false,
+                "hcl" => Regex.Match(value, "^[#]{1}[0-9a-f]{6}$").Success,
+                "ecl" => new string[]{ "amb", "blu", "brn", "gry", "grn", "hzl", "oth" }.Contains(value),
+                "pid" => Regex.Match(value, "^[0-9]{9}$").Success,
+                _ => false
+            };
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var passports = Input.data.Split("\r\n\r\n");
+            PartOne(passports);
+            PartTwo(passports);
+        }
+
+        private static void PartTwo(string[] passports)
+        {
+            int counter = 0;
+            foreach (var passport in passports)
+            {
+                var fields = passport.Trim().Split('\n', ' ');
+                var pass = new Passport();
+                foreach (var field in fields)
+                {
+                    pass.AddField(field.Trim().Split(':')[0], field.Trim().Split(':')[1]);
+                }
+                if (pass.Validate())
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+
+        private static void PartOne(string[] passports)
+        {
+            int counter = 0;
+            foreach (var passport in passports)
+            {
+                var fields = passport.Trim().Split('\n', ' ');
+                int checker = 0;
+                foreach (var field in fields)
+                {
+                    if (field.Trim().Split(':')[0] != "cid")
+                    {
+                        checker++;
+                    }
+                }
+                if (checker == 7)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
